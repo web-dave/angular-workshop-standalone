7515d5164c021086a1135b722e6e7a14fe61cab4
Dave <dave@webdave.de>
2024-09-12 09:47:37 +0200

    use the service

--- src/app/books/book-list/book-list.component.ts
+++ src/app/books/book-list/book-list.component.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, inject, OnInit } from '@angular/core';
+import { BookService } from '../book.service';
 
 @Component({
   selector: 'app-book-list',
   standalone: true,
   imports: [],
   templateUrl: './book-list.component.html',
-  styleUrl: './book-list.component.scss'
+  styleUrl: './book-list.component.scss',
 })
-export class BookListComponent {
+export class BookListComponent implements OnInit {
+  books: any[] = [];
+  service = inject(BookService);
 
+  ngOnInit(): void {
+    this.service.getAll().subscribe((data) => (this.books = data));
+  }
 }
