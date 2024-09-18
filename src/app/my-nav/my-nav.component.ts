b1fade27fc9719d987891fd02e1c01c22994eb54
Dave <dave@webdave.de>
2024-09-18 13:56:44 +0200

    mynav

---
+++ src/app/my-nav/my-nav.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-my-nav',
+  standalone: true,
+  imports: [],
+  templateUrl: './my-nav.component.html',
+  styleUrl: './my-nav.component.scss',
+})
+export class MyNavComponent {}
