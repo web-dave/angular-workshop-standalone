e738de5524d89020f70ba71ef601340891fb1af4
Dave <dave@webdave.de>
2024-09-18 15:58:49 +0200

    routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,14 @@
 import { Routes } from '@angular/router';
+import { BooksComponent } from './books/books.component';
+import { AboutComponent } from './about/about.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: 'books',
+    component: BooksComponent,
+  },
+  {
+    path: 'about',
+    component: AboutComponent,
+  },
+];
--- src/app/my-nav/my-nav.component.ts
+++ src/app/my-nav/my-nav.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
+import { RouterLink, RouterLinkActive } from '@angular/router';
 
 @Component({
   selector: 'app-my-nav',
   standalone: true,
-  imports: [],
+  imports: [RouterLink, RouterLinkActive],
   templateUrl: './my-nav.component.html',
   styleUrl: './my-nav.component.scss',
 })
