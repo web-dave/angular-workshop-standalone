606b3dbf104e255a9f799d8f6aefb22f266da553
Dave <dave@webdave.de>
2024-09-20 12:42:08 +0200

    componentInputBinding

--- src/app/books/book-details/book-details.component.ts
+++ src/app/books/book-details/book-details.component.ts
@@ -1,23 +1,32 @@
-import { Component, inject, OnInit } from '@angular/core';
+import {
+  Component,
+  inject,
+  Input,
+  OnChanges,
+  OnInit,
+  SimpleChanges,
+} from '@angular/core';
 import { BookService } from '../book.service';
 import { ActivatedRoute, RouterLink } from '@angular/router';
 import { IBook } from '../book.interface';
+import { JsonPipe } from '@angular/common';
 
 @Component({
   selector: 'app-book-details',
   standalone: true,
-  imports: [RouterLink],
+  imports: [RouterLink, JsonPipe],
   templateUrl: './book-details.component.html',
   styleUrl: './book-details.component.scss',
 })
-export class BookDetailsComponent implements OnInit {
+export class BookDetailsComponent implements OnInit, OnChanges {
+  @Input() isbn: string = '';
   book?: IBook;
 
   service = inject(BookService);
   route = inject(ActivatedRoute);
-  ngOnInit(): void {
-    this.service
-      .getOne(this.route.snapshot.params['isbn'])
-      .subscribe((data) => (this.book = data));
+  ngOnInit(): void {}
+
+  ngOnChanges(changes: SimpleChanges): void {
+    this.service.getOne(this.isbn).subscribe((data) => (this.book = data));
   }
 }
